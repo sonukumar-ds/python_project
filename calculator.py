ccf938bd9ac36078c65aaa701e56048d72f9596a
Sonu Kumar <sonusaha1962@gmail.com>
2026-02-23 15:01:49 +0530

    Added calculator modules

--- calculator.py
+++ calculator.py
@@ -4,5 +4,5 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-print("Addition:", add(5, 3))
-print("Subtraction:", subtract(5, 3))
+print("Addition:", add(10, 4))
+print("Subtraction:", subtract(10, 4))
