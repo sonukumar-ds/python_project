5d483a13d4cec7b2fd4d261b9a381bdf5cd19d23
Sonu Kumar <sonusaha1962@gmail.com>
2026-02-23 14:54:21 +0530

    Added voting eligibility program

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 # print("Clean Setup Started")
 
-print("Day 1 - Cyber Security Journey")
-print("Next goal: Build real projects")
+# print("Day 1 - Cyber Security Journey")
+# print("Next goal: Build real projects")
+
+age = int(input("enter your age :- "))
+
+if age >= 18:
+    print(f"your age is {age}, so you are Eligible for vote")
+else:
+    print(f"your age is {age}, so you are not Eligible for vote")
+
